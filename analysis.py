e71c19d63aa892b47317a769332dabe2b1208a2c
Bradley Dufour <dufour.bradley@gmail.com>
2018-03-17 14:53:21 -0400

    combines first and mid

--- analysis.py
+++ analysis.py
@@ -20,7 +20,12 @@ racers['birth date'] = temp
 #birth = datetime.datetime.strptime(racers['birth date'], "%m/%d/%y")
 
 races = races[['event', 'start', 'finish', 'runner first', 'runner last']]
-racers = racers[['first', 'last', 'birth date']]
+racers = racers[['first', 'mid', 'last', 'birth date']]
 #%%
+racers['first'] = racers['first'] +' '+ racers['mid'] + '.'
+print(racers)
+racers = racers[['first', 'last', 'birth date']]
 old_racers = racers[racers['birth date'] < datetime.datetime.strptime('1988-01-01', "%Y-%m-%d")]
 young_racers = racers[racers['birth date'] >= datetime.datetime.strptime('1988-01-01', "%Y-%m-%d")]
+races.head()
+print(racers.head())
