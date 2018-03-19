aef65bb972156078c51a51e18d04e28a32ff5744
LordMilton <dufour.bradley@gmail.com>
2018-03-19 14:29:19 -0400

    cleans some garbage data (impossible fast runners) and boxplots by age

--- analysis.py
+++ analysis.py
@@ -25,6 +25,8 @@ racers = racers[['first', 'mid', 'last', 'birth date']]
 races['start'] = pd.to_datetime(races['start'], format='%H:%M:%S')
 races['finish'] = pd.to_datetime(races['finish'], format='%H:%M:%S')
 races['time'] = races['finish'] - races['start']
+#removing anything faster than the fastest recorded marathon time 02:02:57
+races = races[races.time >= datetime.timedelta(hours=2, minutes=2, seconds=57)]
 del(races['finish'])
 del(races['start'])
 print(races)
@@ -39,6 +41,23 @@ young_racers = racers[racers['birth date'] >= datetime.datetime.strptime('1988-0
 old_races = pd.merge(old_racers, races, left_on=['first', 'last'], right_on=['runner first', 'runner last'])
 del(old_races['runner first'])
 del(old_races['runner last'])
+del(old_races['birth date'])
 young_races = pd.merge(young_racers, races, left_on=['first', 'last'], right_on=['runner first', 'runner last'])
 del(young_races['runner first'])
 del(young_races['runner last'])
+del(young_races['birth date'])
+
+#%%
+#analysis
+del(old_races['last'])
+del(old_races['first'])
+del(young_races['last'])
+del(young_races['first'])
+
+#%%
+old_races['age'] = ['old'] * len(old_races.time)
+young_races['age'] = ['young'] * len(young_races.time)
+allRaces_byAge = old_races.append(young_races)
+#%%
+allRaces_byAge.boxplot(column='time', by='age', notch=True)
+print(allRaces_byAge.time.min()) 
